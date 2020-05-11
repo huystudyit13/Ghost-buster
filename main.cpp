be4a30a27d44d87c2959517d44e98734a030265e
huystudyit13 <62603773+huystudyit@users.noreply.github.com>
2020-05-11 13:57:10 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -198,6 +198,7 @@ int main(int argc, char* argv[])
                  }
             }
             if(alive>0) time--;
+            SDL_Delay(30);
             SDL_RenderPresent( gRenderer );
             SDL_Delay(20);
         }
